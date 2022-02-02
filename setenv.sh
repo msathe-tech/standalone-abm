31e3cc935ac5268833523617d79e057c3ef59081
Madhav Sathe <32221454+msathe-tech@users.noreply.github.com>
2022-02-02 17:32:14 -0500

    Update setenv

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# IMP: Please change the ens4 to use correct network interface name in the following command. 
+# It should be the interface which has your IP on the subnet. 
 export CURRENT_IP=$(ip --json a show dev ens4 | jq '.[0].addr_info[0].local' -r)
 echo $CURRENT_IP
 export PATH=$PATH:/home/#replace-me
