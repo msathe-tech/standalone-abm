7c3116f80dd7e8ed21179b40be2a3eca111d35a7
Anjali Khatri <49762723+anjali-khatri@users.noreply.github.com>
2022-02-07 16:32:47 -0500

    Update setenv.sh
    Add a few additional environment variables.

--- setenv.sh
+++ setenv.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# IMP: Please change the ens4 to use correct network interface name in the following command. 
-# It should be the interface which has your IP on the subnet. 
+# IMP: Please change the ens4 to use correct network interface name in the following command.
+# It should be the interface which has your IP on the subnet.
 export CURRENT_IP=$(ip --json a show dev ens4 | jq '.[0].addr_info[0].local' -r)
-echo $CURRENT_IP
-export PATH=$PATH:/home/#replace-me
+echo INSTANCE_IP=$CURRENT_IP
+export ARGOLIS_NAME=ubuntu  ###Replace this value with your Argolis name
+export GCE_CLUSTER_PATH=${ARGOLIS_NAME}_anjalikhatri_altostrat_co
+echo USERNAME=$GCE_CLUSTER_PATH
+export PATH=$PATH:/home/$GCE_CLUSTER_PATH
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/gcloud/application_default_credentials.json
 echo $GOOGLE_APPLICATION_CREDENTIALS
 export PROJECT_ID=$(gcloud config get-value project)
-echo $PROJECT_ID
-#export KUBECONFIG=/home/#replace-me/bmctl-workspace/[#your-cluster-name]/[#your-cluster-name]-kubeconfig
+export CLUSTER_NAME=abm-cluster
+echo PROJECT_ID=$PROJECT_ID
+#export KUBECONFIG=/home/${GCE_CLUSTER_PATH}/bmctl-workspace/${CLUSTER_NAME}/${CLUSTER_NAME}-kubeconfig
 #echo $KUBECONFIG
