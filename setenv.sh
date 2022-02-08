aaf922bbe7b4b596509739a2e275227297c6522a
Anjali Khatri <49762723+anjali-khatri@users.noreply.github.com>
2022-02-08 12:06:38 -0500

    Update setenv.sh
    Adding comments around how to identify the Network Interface name

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # IMP: Please change the ens4 to use correct network interface name in the following command.
 # It should be the interface which has your IP on the subnet.
-export NETWORK_INTERFACE=ens4 #replace this
+export NETWORK_INTERFACE=ens4 ### Replace this value by running and identifying your interface through `ip a`
 export CURRENT_IP=$(ip --json a show dev ${NETWORK_INTERFACE} | jq '.[0].addr_info[0].local' -r)
 echo INSTANCE_IP=$CURRENT_IP
 export ARGOLIS_NAME=ubuntu  ###Replace this value with your Argolis name
